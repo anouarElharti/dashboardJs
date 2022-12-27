5a7d397f029385056ba1b0aff76ebc689defee07
Elharti <AElharti@lims.com>
2022-12-27 17:30:03 +0100

    Adding the monthly chart

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Transactions from "scenes/Transactions";
 import Geography from "scenes/Geography";
 import Overview from "scenes/Overview";
 import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -32,6 +33,7 @@ function App() {
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
             </Route>
           </Routes>
         </ThemeProvider>
